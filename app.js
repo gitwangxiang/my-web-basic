077f1f3b4b99e4c4b24244ddd8d9930d16726238
Hao Hu <hhu@thoughtworks.com>
2015-08-11 14:20:46 +0800

    improve routes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var route = require('./routes/route');
 
 var app = express();
 
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+route.setRoutes(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
